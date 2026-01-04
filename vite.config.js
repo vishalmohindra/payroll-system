4ffa1bc654a972105281e1979fed89a1c1a54290
VishalMohindra <moh.vishal@gmail.com>
2026-01-04 13:32:50 +0530

    Backend MongoDB connected, API working

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/payroll-system/',   // must match repo name exactly
   plugins: [react()],
-});
+  base: '/'
+})
